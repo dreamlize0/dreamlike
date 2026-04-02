16d42228d20c35c922aa823aa43d50419e2bd286
dreamlize0 <dreamlize0@gmail.com>
2026-04-02 07:45:23 +0000

    Upload initial version with current working files

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-// Add JS here
+const generateBtn = document.getElementById('generate-btn');
+const lottoNumbersContainer = document.querySelector('.lotto-numbers');
+
+generateBtn.addEventListener('click', () => {
+    lottoNumbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        numbers.add(randomNumber);
+    }
+
+    const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+
+    sortedNumbers.forEach((number, index) => {
+        setTimeout(() => {
+            const lottoNumber = document.createElement('div');
+            lottoNumber.classList.add('lotto-number');
+            lottoNumber.textContent = number;
+            lottoNumbersContainer.appendChild(lottoNumber);
+        }, index * 100);
+    });
+});
